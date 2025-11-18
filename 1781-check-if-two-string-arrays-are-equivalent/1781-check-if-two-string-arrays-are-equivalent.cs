46474c3a8eac9f401501b1fd1c6c20753e713681
Susan Meduvil Nalakath <80850251+Susan-MN@users.noreply.github.com>
2025-11-18 12:20:45 +0400

    Time: 0 ms (100%), Space: 44.3 MB (35.48%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+    
+     string str1=string.Concat(word1);
+        string str2=string.Concat(word2);
+
+     
+            return   str1==str2;
+       
+    }
+}
