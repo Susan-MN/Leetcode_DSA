4abcfb0f2c13f75c5dd4b69811860dbb016c9fba
Susan Meduvil Nalakath <80850251+Susan-MN@users.noreply.github.com>
2025-12-17 14:06:57 +0400

    Time: 24 ms (64.62%), Space: 29.2 MB (27.69%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int NumberOfMatches(int n) {
+        
+        int matches=0;
+        while(n>1)
+        {
+        if(n%2==0)
+        {
+            matches+=n/2;
+            n=n/2;
+        }else
+        {
+            matches+=(n - 1) / 2;
+            int advance=matches+1;
+            n=(n - 1) / 2 + 1;
+        }
+        }
+        return matches;
+    }
+}
