3f833173d5127d42c58d6e132dd34fa34bcb416e
Susan Meduvil Nalakath <80850251+Susan-MN@users.noreply.github.com>
2026-01-12 17:41:12 +0400

    Time: 37 ms (10.79%), Space: 44.7 MB (9.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        
+        if (s.Length != t.Length)
+            return false;
+        char[] arr1=s.ToCharArray();
+        char[] arr2=t.ToCharArray();
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+        return new string(arr1)==new string(arr2);
+    }
+}
