34de3eafba14e625a4af227ff878fe2096591064
Susan Meduvil Nalakath <80850251+Susan-MN@users.noreply.github.com>
2026-06-15 20:21:59 +0400

    Time: 0 ms (100%), Space: 46.3 MB (58.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cs
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -1,24 +1,37 @@
 public class Solution {
     public void SortColors(int[] nums) {
-    {    
-     for(int i=0;i<nums.Length;i++)
-     {
-        int min=i;
-        for(int j=i+1;j<nums.Length;j++)
+        
+        int start=0;
+        int mid=0;
+        int end=nums.Length-1;
+
+        while(mid<=end)
         {
-            if(nums[min]>nums[j])
+            switch(nums[mid])
             {
-                min=j;
-            
+                case 0:
+                swap(nums,start,mid);
+                mid++;
+                start++;
+                break;
+
+                case 1:
+                mid++;
+                break;
+
+                case 2:
+                swap(nums,mid,end);
+                end--;
+                break;
+                
+
             }
         }
-        int temp=nums[i];
-        nums[i]=nums[min];
-        nums[min]=temp;
-            
-        
-     }  
     }
-   
+    private void swap(int[] nums,int pos1,int pos2 )
+    {
+        int temp=nums[pos1];
+        nums[pos1]=nums[pos2];
+        nums[pos2]=temp;
     }
 }
